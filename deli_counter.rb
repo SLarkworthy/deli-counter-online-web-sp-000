e6c75f5fdcb86bd5118f0d3fe9dcb8933ef261d3
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-01-28 01:04:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     str = "The line is currently: "
-    katz_deli.each_with_index do
+    katz_deli.each_with_index do ||
+    end
   end
 end
